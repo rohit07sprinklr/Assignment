20f5d8e3d3c8404b2316b58d85ca29fd0e792727
rohit kumar <rohit.kumar@sprinklr.com>
2022-05-21 15:18:20 +0530

    Added CSS files

--- script.js
+++ script.js
@@ -8,11 +8,30 @@ return response.json();
     startRendering(data);
 })
 
+function truncateMiddle(word) {
+    const tooLongChars = 30; // arbitrary
+
+    if (word.length < tooLongChars) {
+        return word;
+    }
+
+    const ellipsis = '...';
+    const charsOnEitherSide = Math.floor(tooLongChars / 2) - ellipsis.length;
+
+    return word.slice(0, charsOnEitherSide) + ellipsis + word.slice(-charsOnEitherSide);
+} 
+
 function markup(arrayobject,objectid){
     return`
-    <div class="listitem" id="${objectid}">
-        <p onclick="updateimage(${objectid})"><img src="${arrayobject.previewImage}" class="logo"></img> ${arrayobject.title}</p>
+    <div class="listitem" id="${objectid}" onclick="updateimage(${objectid})">
+    <p class="listitemtext"><img src="${arrayobject.previewImage}" class="logo"></img>${truncateMiddle(arrayobject.title)}</p>
     </div>
+    `
+}
+function textboxmarkup(){
+    const textbox = document.querySelector(".textbox");
+    textbox.innerHTML= `
+    ${dataArray[currentID].title}
     `
 }
 
@@ -23,38 +42,42 @@ function Renderimage(){
 
 function updateimage(objectid){
     console.log(objectid);
-    // const imagelist = document.querySelectorAll(".listitem");
-    // imagelist.forEach(element =>{
-    //     element.style.backgroundColor = "white";
-    // });
     currentID=objectid;
-    // curimage = document.getElementById(currentID);
-    // curimage.style.backgroundColor = "blue";
     Renderimage();
+    textboxmarkup()
+}
+const inputHandler = function(event) {
+    console.log(event.srcElement.innerText);
+    // dataArray[currentID].title = event.srcElement.innerHTML;
+    var current = document.getElementsByClassName("active");
+    // var curele = document.querySelector(".active");
+    console.log(current[0].innerHTML);
+    // curele.textContent= event.srcElement.innerText;
+    current[0].innerHTML =`
+    <div class="listitem" id="${currentID}" onclick="updateimage(${currentID})">
+    <p class="listitemtext"><img src="${dataArray[currentID].previewImage}" class="logo"></img>${truncateMiddle(event.srcElement.innerText)}</p>
+    </div>
+    `;
+
 }
 
 function logKey(event) {
     if(event.code=='ArrowDown'&&currentID+1<dataArray.length){
-        // let curimage = document.getElementById(currentID);
-        // curimage.style.backgroundColor = "white";
         currentID+=1;
         Renderimage();
+        textboxmarkup()
     }
     else if(event.code=='ArrowUp'&&currentID>0){
-        // let curimage = document.getElementById(currentID);
-        // curimage.style.backgroundColor = "white";
         currentID-=1;
         Renderimage();
+        textboxmarkup()
     }
-    // curimage = document.getElementById(currentID);
-    // curimage.style.backgroundColor = "blue";
     var elements= document.getElementsByClassName("listitem");
     
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     elements[currentID].className += " active";
 }
-
 currentID=0;
 var dataArray=0;
 
@@ -71,6 +94,7 @@ function startRendering(data){
     var elements= document.getElementsByClassName("listitem");
     elements[0].className += " active";
     Renderimage();
+    textboxmarkup()
     for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener("click", function() {
         var current = document.getElementsByClassName("active");
@@ -79,7 +103,8 @@ function startRendering(data){
     });
     }
 
+    const textbox = document.getElementById('textboxid');
+    textbox.addEventListener('input', inputHandler);
     document.addEventListener('keydown', logKey);
 }
-
 
